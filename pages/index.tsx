ad93c7dbc86cbabd7808e8b6aa76d7660d10707d
Matthew Pagan <mastapegs01@gmail.com>
2021-09-22 17:19:08 -0400

    better code

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,9 @@ import { Mesh, MeshStandardMaterial, BoxGeometry } from "three";
 
 interface BoxProps {
   position: [number, number, number];
+  spin?: "x" | "y" | "z";
 }
-const Box: FC<BoxProps> = ({ position }) => {
+const Box: FC<BoxProps> = ({ position, spin = "x" }) => {
   const mesh = useRef<Mesh<BoxGeometry, MeshStandardMaterial>>(null);
 
   const [hovered, setHover] = useState(false);
@@ -13,7 +14,7 @@ const Box: FC<BoxProps> = ({ position }) => {
 
   useFrame(() => {
     if (!mesh || !mesh.current) return;
-    mesh.current.rotation.y += 0.01;
+    mesh.current.rotation[spin] += 0.01;
   });
 
   return (
@@ -32,16 +33,14 @@ const Box: FC<BoxProps> = ({ position }) => {
 };
 
 const Index: FC = () => (
-  <div className="min-h-screen bg-green-200">
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-5, -1, 0]} />
-      <Box position={[-2, -1, 0]} />
-      <Box position={[2, -1, 0]} />
-      <Box position={[5, -1, 0]} />
-    </Canvas>
-  </div>
+  <Canvas className="min-h-screen">
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <Box spin="x" position={[-3, 0, 0]} />
+    <Box spin="y" position={[-1, 0, 0]} />
+    <Box spin="z" position={[1, 0, 0]} />
+    <Box position={[3, 0, 0]} />
+  </Canvas>
 );
 
 export default Index;
